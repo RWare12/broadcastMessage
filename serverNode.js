1e9d17d4c3ea4825c89499292a46600b8d326da8
kinghoratiq <king_horatiq69@yahoo.com>
2018-07-26 11:27:52 +0800

    added push messaging for facebook and telegram

--- serverNode.js
+++ serverNode.js
@@ -3,29 +3,34 @@ const app = express();
 const path = require('path');
 let bodyParser = require('body-parser');
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get('/', function (req, res){
-
     res.sendFile(path.join(__dirname + '/index.html'));
-
 });
 
 // This responds a POST request for the homepage
 app.post('/sendMessage', (req, res) => {
-        // using fb-messenger api to send message to users
+       
+    // using fb-messenger api to send message to users
     let broadcastMessage = req.body.message;
     let FBMessenger = require('fb-messenger');
-    let messenger = new FBMessenger("EAAFQIPkB2ZCYBAOdx1zhYAQJrGk9e1Hj2RFj5jVmfapJYqgAlKe9cJakg44Y1Vy9dzYEbj1FbvgKsDpGyQVIyGJDG1Pelbtslxf9wzoCwkBfNtw71jtxvQHv0YLuq0t62g3t5yCtY33BZBDjrckZCH49eDeca92vXov2uKoFQZDZD");
+    let messenger = new FBMessenger("EAAFpsSI1Pa8BAMqryJlniwRwu14cu62thAMbB65BFYZBI9Wo5kVXNI9WbQ9AxbJLhY7fttZCy1ZBN8FZCmolNDVpf5nqi0vMNrPMoeyZBAqiZBAaZBWvJXZBlv3ZCp65zIry13EPiq3kdT0chJCVellVMNRBTFtJUbJdv2fskX8p2tAZDZD");
   
-        messenger.sendTextMessage(1721885347926612, broadcastMessage, function (err, body) {
+        messenger.sendTextMessage(2066657673406251, broadcastMessage, function (err, body) {
             if(err) return console.error(err)
             console.log(body);
         });
-    
 
-    res.send('Message has been broadcast.');
+    //Telegram push implementation
+    const { TelegramClient } = require('messaging-api-telegram');
+	const client = TelegramClient.connect('696494774:AAHCl0iDVjrlW9SJKMrBGYH0Wg2derP_UDI');
+	client.sendMessage(630541527, broadcastMessage, {
+		disable_web_page_preview: true,
+		disable_notification: true,
+	});
+
+	res.send('Message has been broadcast.');
 });
 
 
